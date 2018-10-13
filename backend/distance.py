0247eac2a14938871f6268fdceed7de535996ece
coraxyc <ycxing99@gmail.com>
2018-10-13 13:27:06 -0700

    Added API key

--- backend/distance.py
+++ backend/distance.py
@@ -8,4 +8,4 @@ def getDistance(startLoc, endLoc):
 
     # https://maps.googleapis.com/maps/api/distancematrix/json?origins=
     # Seattle&destinations=San+Francisco&key=INSERT_API_KEY
-    maps_key = API_KEY;
+    maps_key = 'AIzaSyBaDoxmA0conei-WUGZHS6moh7o_YphxCQ'
