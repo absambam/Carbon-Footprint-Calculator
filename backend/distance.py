74edc382505d608ace7982c828a767acd685ca70
annsudhart <sudharta.andrea@gmail.com>
2018-10-13 13:53:58 -0700

    Implemented Google DistanceMatrix API

--- backend/distance.py
+++ backend/distance.py
@@ -8,4 +8,36 @@ def getDistance(startLoc, endLoc):
 
     # https://maps.googleapis.com/maps/api/distancematrix/json?origins=
     # Seattle&destinations=San+Francisco&key=INSERT_API_KEY
+    # TODO: replace spaces with plus signs in startLoc and endLoc
+
     maps_key = 'AIzaSyBaDoxmA0conei-WUGZHS6moh7o_YphxCQ'
+    base_url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+
+    url = base_url + '?' + urllib.urlencode({
+        'origins': startLoc,
+        'destinations': endLoc,
+        'key': maps_key,
+    })
+
+    current_delay = 0.1
+    max_delay = 3600
+
+    while True:
+        try:
+            response = str(urllib2.urlopen(url).read())
+        except IOError:
+            pass #Fall thru
+        else:
+            result = json.loads(response.replace('\\n', ''))
+            if result['status']  == 'OK':
+                return ['rows']['elements']['distance']['text']
+            elif result['status'] != 'UNKNOWN_ERROR':
+                raise Exception(result['error_message'])
+
+        if current_delay > max_delay:
+            raise Exception('Too many retry attempts. :(')
+        print('Waiting', current_delay, 'seconds before retrying...')
+        time.sleep(current_delay)
+        current_delay *= 2
+
+print(getDistance('UC+San+Diego', 'Vallartas+San+Diego'))
