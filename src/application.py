e164915eee0de872edb388308fc9d329183cb2cc
absambam <samabi27@gmail.com>
2018-10-13 23:24:21 -0700

    added the numTrees and nPHPY functions into the main application.py

--- src/application.py
+++ src/application.py
@@ -6,30 +6,35 @@ import emission_calc
 
 app = Flask(__name__)
 
+# open the main screen
 @app.route("/")
 def start():
     url_for('static', filename='main.css')
     return render_template('index.html')
 
+# open the search screen
 @app.route("/app.html")
 def lookup():
     url_for('static', filename='app.css')
     return render_template('app.html')
 
+# run through and calculate everything
 @app.route("/app", methods=['POST', 'GET'])
 def calc():
-    print('here')
+    error=None
     if request.method == 'POST':
         if request.form['starting-point'] != None and request.form['destination'] != None and request.form['mpg'] != '0':
             dist = distance.getDistance(request.form['starting-point'], request.form['destination'])
             print (dist)
-            if float(dist) == -1:
+            if dist == -1:
                 return render_template('app.html', error=error)
             emission = emission_calc.calcFootprint(request.form['mpg'], dist)
             # monthTravels = emission_calc.numTravelsInMonth(emission)
             yearTravels = emission_calc.numTravelsInYear(emission)
+            trees = emission_calc.numTrees(emission)
+            homes = emission_calc.numPerHomePerYear(emission)
 
-            data = (dist, emission, yearTravels)
+            data = (emission, homes, trees, yearTravels)
             return render_template('test.html', data=data)
     return render_template('app.html', error=error)
 
