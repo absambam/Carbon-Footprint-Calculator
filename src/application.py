54e8d93280769e5f5c8e25cbb39f0fc217b3fc0e
absambam <samabi27@gmail.com>
2018-10-14 00:18:07 -0700

    added comments for application.py and emission_calc.py

--- src/application.py
+++ src/application.py
@@ -21,21 +21,29 @@ def lookup():
 # run through and calculate everything
 @app.route("/app", methods=['POST', 'GET'])
 def calc():
-    error=None
+    error=None # for error returns
+    # if there's some sort of input from the website
     if request.method == 'POST':
+        # check if any of the necessary variables are uninitialized
         if request.form['starting-point'] != None and request.form['destination'] != None and request.form['mpg'] != '0':
+
+            # get the distance from the starting point to destination
             dist = distance.getDistance(request.form['starting-point'], request.form['destination'])
-            print (dist)
+            # make sure a valid distance was given before running calculations
             if dist == -1:
                 return render_template('app.html', error=error)
+
+            # calculate the necessary variables
             emission = emission_calc.calcFootprint(request.form['mpg'], dist)
-            # monthTravels = emission_calc.numTravelsInMonth(emission)
             yearTravels = emission_calc.numTravelsInYear(emission)
             trees = emission_calc.numTrees(emission)
             homes = emission_calc.numPerHomePerYear(emission)
 
+            # store the calculated data in a tuple and return it to the end html file
             data = (emission, homes, trees, yearTravels)
             return render_template('test.html', data=data)
+
+    # return error if there was an issue with any of the inputted values
     return render_template('app.html', error=error)
 
-app.run()
+app.run() # this is pretty self-explanatory...
