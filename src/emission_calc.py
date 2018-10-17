5f20d61b0dc4a7dc0711a800def6ee3623e5b0a6
absambam <samabi27@gmail.com>
2018-10-16 22:11:08 -0700

    removed numTravelsInMonth since we aren't going to use that calculation

--- src/emission_calc.py
+++ src/emission_calc.py
@@ -10,12 +10,6 @@ def numTravelsInYear(emission):
     travels = int(16.491 * 2204.62 / int(float(emission)))
     return travels
 
-def numTravelsInMonth(emission):
-    # the number of travels to given place one would complete in a month to get
-    # the average per-capita carbon emissions in the U.S.
-    travels = int(1.374 * 2204.62 / int(float(emission)))
-    return travels
-
 def numPerHomePerYear(emission):
     # comparison with household carbon consumption per year
     houses = int((9.26*2205) / int(float(emission)))
